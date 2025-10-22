35042143d402bdd73045b47be272e88716fe77bb
Logan Cacy <167669218+LoganCacy@users.noreply.github.com>
2025-10-21 21:44:04 -0700

    Update get_data.php

--- Assignment_4:[PHP & Databasing]/get_data.php
+++ Assignment_4:[PHP & Databasing]/get_data.php
@@ -2,10 +2,10 @@
 header('Content-Type: application/json');
 
 // Step 1: Database connection details
-$servername = "localhost";  // On Hostinger, this may look like "mysql.hostinger.com"
-$username = "u137220217_db_logancacy";  // <-- replace with your Hostinger database username
-$password = "S@lty123";  // <-- replace with your Hostinger database password
-$dbname = "u137220217_logancacy";  // <-- replace with your Hostinger database name
+$servername = "localhost";  
+$username = ;  
+$password = ;  
+$dbname = ;  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
